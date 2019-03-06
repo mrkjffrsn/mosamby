329efb0114776c6e27c7438102819112559bd3ac
Mark Jefferson <tufjef@gmail.com>
2019-03-06 00:37:08 -0500

    Including docs to host the main site on github pages

---
+++ docs/precache-manifest.a687434c38bb429373d93d7e7083fcf7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "efb2fc2bb9f06c4203f5",
+    "url": "/static/js/main.efb2fc2b.chunk.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "efb2fc2bb9f06c4203f5",
+    "url": "/static/css/main.00664c25.chunk.css"
+  },
+  {
+    "revision": "6b0451b4f99e79cad97f93a7b3944772",
+    "url": "/index.html"
+  }
+];
